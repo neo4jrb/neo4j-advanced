2e700f170f6d83cccc8644f8142e85f784ffbeee
Andreas Ronge <andreas.ronge@gmail.com>
2012-01-12 15:37:57 +0100

    Release of 1.6.0.alpha.1, make sure NEO_VERSION is not defined twice

--- lib/neo4j-advanced/version.rb
+++ lib/neo4j-advanced/version.rb
@@ -1,12 +1,14 @@
 module Neo4j
   module Advanced
-    VERSION = "1.6.0.alpha.3"
+    VERSION = "1.6.0.alpha.1"
     NEO_VERSION = "1.6.M02"
   end
 
   # make sure community, advanced and enterprise neo4j jar files have the same version
-  if defined?(NEO_VERSION) && NEO_VERSION != Advanced::NEO_VERSION
-    raise "Mismatch of Neo4j JAR versions. Already loaded JAR files #{NEO_VERSION}, neo4j-advanced: #{Advanced::NEO_VERSION}" 
+  if defined?(NEO_VERSION) 
+    if NEO_VERSION != Advanced::NEO_VERSION
+      raise "Mismatch of Neo4j JAR versions. Already loaded JAR files #{NEO_VERSION}, neo4j-advanced: #{Advanced::NEO_VERSION}" 
+    end
   else
     NEO_VERSION = Advanced::NEO_VERSION
   end
